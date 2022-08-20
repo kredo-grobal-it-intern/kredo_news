0b7de13c0d409e5a4e42538b348aa9bb8749e6a6
Raymon Bilocura <rbilocura730@gmail.com>
2022-08-20 14:10:50 +0800

    fix npm run dev error

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/assets/sass/app.scss', 'public/css')
-    .sass('resources/assets/sass/admin.scss', 'public/css')
-    .sass('resources/assets/sass/profile.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/js').vue()
+    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/admin.scss', 'public/css')
+    .sass('resources/sass/profile.scss', 'public/css');
