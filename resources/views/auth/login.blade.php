9264b8b71452140d881751d7ea8b9b4eeb726a1a
Seiichiro <skyblue1011@icloud.com>
2022-08-20 16:53:34 +0900

    Fix the file for login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,13 +1,15 @@
 @extends('layouts.app')
 
 @section('body_id', 'back-blue')
-
+@section('style')
+<link href="{{ asset('css/login.css') }}" rel="stylesheet">
+@endsection
 @section('content')
 <div class="container-fluid d-flex justify-content-center text-center" style="margin-top:100px;">
-    <div class="col-4">
+    <div class="col-4 mx-auto">
         <img src="{{asset('/storage/image/logo3.PNG')}}" alt="logo2" style="height:auto; width:100px;">
         <p class="h4 text-white text-center">Log in to your CCC account</p>
-        <div class="form-check text-white mb-4 text-center">
+        <div class="form-check text-white mb-4 text-center ms-0">
             <p class="ms-0">Not member yet?
                 <a href="" class="text-white">Sign up</a>
             </p>
@@ -15,16 +17,20 @@
             <form method="POST" action="{{ route('login') }}">
                 @csrf
                 <div class="row mb-3">
-                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="E-mail" required autocomplete="email" autofocus>
+                    <div class="col-md-10 offset-md-1">
 
-                    @error('email')
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="E-mail" required autocomplete="email" autofocus>
+
+                        @error('email')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
-                    @enderror
+                        @enderror
 
+                    </div>
                 </div>
                 <div class="row mb-3">
+                    <div class="col-md-10 offset-md-1">
                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Password" required autocomplete="current-password">
 
                         @error('password')
@@ -33,11 +39,14 @@
                             </span>
                         @enderror
 
+                    </div>
                 </div>
                 <div class="row mb-0">
-                    <button type="submit" class="btn btn-danger fw-bold" style="width:100%">
+                    <div class="col-md-10 offset-md-1">
+                    <button type="submit" class="btn btn-danger fw-bold w-100">
                             {{ __('Login') }}
                     </button>
+                    </div>
                 </div>
         </form>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('style')
 </head>
 <body id="@yield('body_id','deafault')">
     <div id="app">
