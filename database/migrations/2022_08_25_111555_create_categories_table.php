e1ad91af8c2571338a5bb401a9d2281b077750d6
rbilocura123 <33739983+rbilocura123@users.noreply.github.com>
2022-09-05 15:01:34 +0800

    Update 2022_08_25_111555_create_categories_table.php

--- database/migrations/2022_08_25_111555_create_categories_table.php
+++ database/migrations/2022_08_25_111555_create_categories_table.php
@@ -18,7 +18,6 @@ class CreateCategoriesTable extends Migration
             $table->string('name');
             $table->string('image');
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
