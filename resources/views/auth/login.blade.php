537c2bbfb11e09561e1bcb89587e771e1d6d2a3f
Seiichiro <skyblue1011@icloud.com>
2022-09-08 19:41:51 +0900

    minor fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
 <div class="container text-center container-custom">
     <div class="row">
         <div class="col-lg-6 col-md-10 col-sm-12 mx-auto">
-            <img src="{{ mix('images/logo3.PNG') }}" alt="logo3" class="logo">
+            <img src="{{ asset('images/logo3.PNG') }}" alt="logo3" class="logo">
             <p class="h4 text-center">Log in to your CCC acount</p>
             <p>
                 <span>Not member yet?</span>
