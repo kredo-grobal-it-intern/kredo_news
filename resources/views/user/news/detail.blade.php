efb9c53901f309336b03949e39b4daae662b7905
shuko <standup4urlife@gmail.com>
2022-09-10 14:48:47 +0900

    detail_page_rearrange_dummy_images

--- resources/views/user/news/detail.blade.php
+++ resources/views/user/news/detail.blade.php
@@ -5,7 +5,7 @@
     <div class="col-8">
         <div class="card ms-5 border-0 bg-transparent">
             <div class="card-header bg-transparent border-bottom-0">
-{{-- ------------------------------------News section--------------------------------------- --}}
+{{-- Left side: News --}}
                 <h1 class="text-decoration-underline fw-bold"> {{$news->title}}</h1>
                 <div class="row m-0 py-0">
                     <p>News site: {{$news->source_name}}</p>
@@ -14,12 +14,12 @@
                     <p>Published: {{$news->published_at}}</p>
                 </div>
                 <div class="row m-0 py-0">
-                   <p class="fw-bold"> {{$news->author}}</p>
+                    <p class="fw-bold"> {{$news->author}}</p>
                 </div>
             </div>
 
             <div class="card-body justify-content-center mx-auto">
-                <img src="{{ asset('storage/image/' . $news->image_path) }}" alt="">
+                <img src="{{ asset('storage/images/dummy.jpg') }}" alt="">
                 <p>caption for image? description?</p>
                 <p>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum nisi fugiat ad similique et placeat neque. Aliquam nulla odit assumenda dignissimos sit quibusdam, optio ab delectus maxime fugit esse adipisci.
@@ -27,7 +27,7 @@
                 <p>
                     URL: {{$news->url}}
                 </p>
-{{-- ----------------------------------Comment section------------------------- --}}
+{{-- Comments section --}}
                 <div class="fw-bold mt-5"> Comment</div>
                 {{-- @foreach() --}}
                 <div class="row mt-3">
@@ -41,7 +41,7 @@
                     </div>
                     <div class="col-1 pe-2">
                         <i class="fa-solid fa-ellipsis"></i>
-                        <i class="fa-solid fa-thumbs-up">100</i>
+                        <a href="" class="me-2 text-decoration-none text-dark">1000 <i class="fa-regular fa-thumbs-up"></i></a>
                     </div>
                 </div>
                 {{-- @endforeach --}}
@@ -57,7 +57,7 @@
                     </div>
                     <div class="col-1 pe-2">
                         <i class="fa-solid fa-ellipsis"></i>
-                        <i class="fa-solid fa-thumbs-up">100</i>
+                        <a href="" class="me-2 text-decoration-none text-dark">1000 <i class="fa-regular fa-thumbs-up"></i></a>
                     </div>
                 </div>
                 <hr>
@@ -72,7 +72,7 @@
                     </div>
                     <div class="col-1 pe-2">
                         <i class="fa-solid fa-ellipsis"></i>
-                        <i class="fa-solid fa-thumbs-up">100</i>
+                        <a href="" class="me-2 text-decoration-none text-dark">1000 <i class="fa-regular fa-thumbs-up"></i></a>
                     </div>
                 </div>
                 <hr>
@@ -87,7 +87,7 @@
                     </div>
                     <div class="col-1 pe-2">
                         <i class="fa-solid fa-ellipsis"></i>
-                        <i class="fa-solid fa-thumbs-up">100</i>
+                        <a href="" class="me-2 text-decoration-none text-dark">1000 <i class="fa-regular fa-thumbs-up"></i></a>
                     </div>
                 </div>
                 <form action="#">
@@ -101,94 +101,55 @@
         </div>
     </div>
 
-{{-- ------------------------------------Right side--------------------------------------- --}}
+{{-- Right side --}}
     <div class="col-4">
         <div class="card-body me-3">
-{{--  -----------------------------------Whats hot--------------------- --}}
+{{-- Whats hot --}}
         <hr>
-          <h2 class="fw-bold text-decoration-underline">What's Hot</h2>
+            <h2 class="fw-bold text-decoration-underline">What's Hot</h2>
         <hr>
-          {{-- @foreach ( as ) --}}
-             <div class="row">
-                <div class="col-5 my-auto mx-auto ps-4">
-                    <img src="{{ asset('storage/image/' . $news->image_path) }}" alt="" style="width:100px">
-                </div>
-                <div class="col-7">
-                    <p class="fw-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit possimus cum tempora</p>
-                    <div class="float-end me-3">
-                        <i class="fa-solid fa-thumbs-up">100</i>
-                        <i class="fa-solid fa-thumbs-down">50</i>
-                        <i class="fa-solid fa-comment-dots">70</i>
-                    </div>
-                </div>
-             </div>
-             <hr>
-             <div class="row">
-                <div class="col-5 my-auto mx-auto ps-4">
-                    <img src="{{ asset('storage/image/' . $news->image_path) }}" alt="" style="width:100px">
-                </div>
-                <div class="col-7">
-                    <p class="fw-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit possimus cum tempora</p>
-                    <div class="float-end me-3">
-                        <i class="fa-solid fa-thumbs-up">100</i>
-                        <i class="fa-solid fa-thumbs-down">50</i>
-                        <i class="fa-solid fa-comment-dots">70</i>
-                    </div>
-                </div>
-             </div>
-             <hr>
-             <div class="row">
-                <div class="col-5 my-auto mx-auto ps-4">
-                    <img src="{{ asset('storage/image/' . $news->image_path) }}" alt="" style="width:100px">
-                </div>
-                <div class="col-7">
-                    <p class="fw-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit possimus cum tempora</p>
-                    <div class="float-end me-3">
-                        <i class="fa-solid fa-thumbs-up">100</i>
-                        <i class="fa-solid fa-thumbs-down">50</i>
-                        <i class="fa-solid fa-comment-dots">70</i>
-                    </div>
-                </div>
-             </div>
-          {{-- @endforeach --}}
-          <hr>
-{{-- ----------------------------------latest in--------------------------------------------- --}}
-          <hr>
+        @include('user.news.top-body.whats_hot')
+{{-- latest in --}}
+        <hr>
             <h2 class="fw-bold text-decoration-underline">Latest In <span class="fw-bold">{{$news->source_name}}</span></h2>
-          <hr>
-          <div class="row">
+        <hr>
+        <div class="row">
             <div class="col">
                 <p class="text-decoration-underline fw-bold"> {{$news->title}}</p>
-                <div class="float-end me-3">
-                    <i class="fa-solid fa-thumbs-up">100</i>
-                    <i class="fa-solid fa-thumbs-down">50</i>
-                    <i class="fa-solid fa-comment-dots">70</i>
-                </div>
+                <p>
+                    <a href="" class="me-2 text-decoration-none text-dark">1000 <i class="fa-regular fa-thumbs-up"></i></a>
+                    <a href="" class="me-2 text-decoration-none text-dark">200 <i class="fa-regular fa-thumbs-down"></i></a>
+                    <a href="" class="me-2 text-decoration-none text-dark">100 <i class="fa-regular fa-comment-dots"></i></a>
+                    <a href="" class="text-decoration-none text-dark"><i class="fa-regular fa-bookmark"></i></a>
+                </p>
             </div>
-          </div>
-          <hr>
-          <div class="row">
+        </div>
+        <hr>
+        <div class="row">
             <div class="col">
                 <p class="text-decoration-underline fw-bold"> {{$news->title}}</p>
-                <div class="float-end me-3">
-                    <i class="fa-solid fa-thumbs-up">100</i>
-                    <i class="fa-solid fa-thumbs-down">50</i>
-                    <i class="fa-solid fa-comment-dots">70</i>
-                </div>
+                <p>
+                    <a href="" class="me-2 text-decoration-none text-dark">1000 <i class="fa-regular fa-thumbs-up"></i></a>
+                    <a href="" class="me-2 text-decoration-none text-dark">200 <i class="fa-regular fa-thumbs-down"></i></a>
+                    <a href="" class="me-2 text-decoration-none text-dark">100 <i class="fa-regular fa-comment-dots"></i></a>
+                    <a href="" class="text-decoration-none text-dark"><i class="fa-regular fa-bookmark"></i></a>
+                </p>
             </div>
-          </div>
-          <hr>
-          <div class="row">
+        </div>
+        <hr>
+        <div class="row">
             <div class="col">
                 <p class="text-decoration-underline fw-bold"> {{$news->title}}</p>
-                <div class="float-end me-3">
-                    <i class="fa-solid fa-thumbs-up">100</i>
-                    <i class="fa-solid fa-thumbs-down">50</i>
-                    <i class="fa-solid fa-comment-dots">70</i>
+                <p>
+                    <a href="" class="me-2 text-decoration-none text-dark">1000 <i class="fa-regular fa-thumbs-up"></i></a>
+                    <a href="" class="me-2 text-decoration-none text-dark">200 <i class="fa-regular fa-thumbs-down"></i></a>
+                    <a href="" class="me-2 text-decoration-none text-dark">100 <i class="fa-regular fa-comment-dots"></i></a>
+                    <a href="" class="text-decoration-none text-dark"><i class="fa-regular fa-bookmark"></i></a>
+                </p>
                 </div>
             </div>
-          </div>
-          <hr>
+        </div>
+        <hr>
         </div>
     </div>
 </div>
