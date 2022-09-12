8be87177c9b4855650da2f5381a9331bd7fb53e2
harufukuda <harufukuda14@gmail.com>
2022-09-12 23:21:53 +0900

    add error message on comment

--- resources/views/user/news/detail.blade.php
+++ resources/views/user/news/detail.blade.php
@@ -58,7 +58,10 @@
                 <form action="{{ route('user.comment.store', $news->id) }}" method="post">
                     @csrf
                     <div class="mb-3 mt-5">
-                        <textarea class="form-control mt-3" name="comment" id="comment" rows="3"></textarea>
+                        <textarea class="form-control mt-3" name="comment" id="comment" rows="3">{{ old('comment') }}</textarea>
+                        @error('comment')
+                            <small class="text-danger">{{ $message }}</small>
+                        @enderror
                         <button type="submit" class="btn btn-outline-secondary btn-sm mt-2 float-end">Post Comment</button>
                     </div>
                 </form>
