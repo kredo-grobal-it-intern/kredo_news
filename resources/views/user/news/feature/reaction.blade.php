0a7a115d7b89e451ed41e31ca1b24d9de185a9d5
harufukuda <harufukuda14@gmail.com>
2022-09-17 01:01:18 +0900

    edit reaction styling

--- resources/views/user/news/feature/reaction.blade.php
+++ resources/views/user/news/feature/reaction.blade.php
@@ -1,23 +1,22 @@
 @if (Auth::check() && $news->isUp())
-    <p class="col offset-1 fs-5">
-        <span class="upCount">{{ $news->like_reactions()->count() > 0 ?? '' }}</span>
-        <i class="fa-regular fa-thumbs-up up-toggle text-primary" data-newsid="{{ $news->id }}"></i>
+    <p class="me-2">
+        <span class="upCount">{{ $news->like_reactions()->count() }}</span>
+        <i class="reaction fa-regular fa-thumbs-up up-toggle text-primary" data-newsid="{{ $news->id }}"></i>
     </p>
 @else
-    <p class="col offset-1 fs-5">
-        <span class="upCount">{{ $news->like_reactions()->count() > 0 ?? '' }}</span>
-        <i class="fa-regular fa-thumbs-up up-toggle" data-newsid="{{ $news->id }}"></i>
+    <p class="me-2">
+        <span class="upCount">{{ $news->like_reactions()->count() }}</span>
+        <i class="reaction fa-regular fa-thumbs-up up-toggle" data-newsid="{{ $news->id }}"></i>
     </p>
 @endif
 @if (Auth::check() && $news->isDown())
-    <p class="col  fs-5">
-        <span class="downCount">{{ $news->dislike_reactions()->count() > 0 ?? '' }}</span>
-        <i class="fa-regular fa-thumbs-down down-toggle text-primary" data-newsid="{{ $news->id }}"></i>
+    <p class="me-2">
+        <span class="downCount">{{ $news->dislike_reactions()->count() }}</span>
+        <i class="reaction fa-regular fa-thumbs-down down-toggle text-primary" data-newsid="{{ $news->id }}"></i>
     </p>
 @else
-    <p class="col  fs-5">
-        <span class="downCount">{{ $news->dislike_reactions()->count() > 0 ?? '' }}</span>
-        <i class="fa-regular fa-thumbs-down down-toggle" data-newsid="{{ $news->id }}"></i>
+    <p class="me-2">
+        <span class="downCount">{{ $news->dislike_reactions()->count() }}</span>
+        <i class="reaction fa-regular fa-thumbs-down down-toggle" data-newsid="{{ $news->id }}"></i>
     </p>
 @endif
-<p class="col fs-5">100 <i class="fa-regular fa-comment-dots"></i></p>
--- resources/views/user/news/layouts/news_list.blade.php
+++ resources/views/user/news/layouts/news_list.blade.php
@@ -3,9 +3,9 @@
     <a href="{{ route('news.show', $news->id) }}"><img src="{{ asset('images/news/' . $news->image) }}" alt="News Image" class="news-list-image w-100"></a>
     <h5 class="sub-heading fw-bold mt-3"><a href="{{ route('news.show', $news->id) }}" class="text-danger text-decoration-none">{{ $news->title }}</a></h5>
     <p class="sub-description">{{ $news->description }}</p>
-    <div class="row mt-1">
+    <div class="d-flex mt-1">
         @include('user.news.feature.reaction')
-        <a href="" class="me-2 text-decoration-none text-dark">{{ $news->comments->count() }} <i class="fa-regular fa-comment-dots"></i></a>
+        <p class="me-2">{{ $news->comments->count() }} <i class="fa-regular fa-comment-dots"></i></p>
         @include('user.news.feature.bookmark')
     </div>
 </div>
