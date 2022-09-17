e8f0eb5333e7412eaf1e53eb8c85e382474964b7
Seiichiro <skyblue1011@icloud.com>
2022-09-17 12:08:58 +0900

    Class tag has been changed, so adjusted for new design

--- resources/views/user/news/layouts/news_list.blade.php
+++ resources/views/user/news/layouts/news_list.blade.php
@@ -3,7 +3,7 @@
     <a href="{{ route('news.show', $news->id) }}"><img src="{{ asset('images/news/' . $news->image) }}" alt="News Image" class="news-list-image w-100"></a>
     <h5 class="sub-heading fw-bold mt-3"><a href="{{ route('news.show', $news->id) }}" class="text-danger text-decoration-none">{{ $news->title }}</a></h5>
     <p class="sub-description">{{ $news->description }}</p>
-    <div class="d-flex mt-1">
+    <div class="d-flex mt-1 status">
         @include('user.news.feature.reaction')
         <p class="me-2">{{ $news->comments->count() }} <i class="fa-regular fa-comment-dots"></i></p>
         @include('user.news.feature.bookmark')
