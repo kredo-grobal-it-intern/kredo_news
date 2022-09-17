d187b8ebb3dca78180d95e5610d062562c916c82
Gityu2 <ca150animly@gmail.com>
2022-09-17 17:33:01 +0900

    Fix your indicate

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -9,19 +9,19 @@ class FollowController extends Controller
 {
     public function destroyFollower($follower_id)
     {
-        $follower = Follow::where('follower_id', $follower_id)
-                        ->where('following_id', Auth::user()->id);
-        
-        $follower->delete();
+        Follow::where('follower_id', $follower_id)
+                ->where('following_id', Auth::user()->id)
+                ->delete();
+
         return redirect()->back();
     }
 
     public function destroyFollowing($following_id)
     {
-        $following = Follow::where('follower_id', Auth::user()->id)
-                        ->where('following_id', $following_id);
+        Follow::where('follower_id', Auth::user()->id)
+                ->where('following_id', $following_id)
+                ->delete();
         
-        $following->delete();
         return redirect()->back();
     }
 }
