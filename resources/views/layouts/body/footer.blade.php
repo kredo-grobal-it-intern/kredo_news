4ce34b2f39db26a433b6aa5c9675aae295afaea4
harufukuda <harufukuda14@gmail.com>
2022-09-20 22:05:24 +0900

    footer responsive

--- resources/views/layouts/body/footer.blade.php
+++ resources/views/layouts/body/footer.blade.php
@@ -1,91 +1,91 @@
-<div class="container">
+<div class="container p-4 p-sm-0">
     <!-- Grid row -->
-    <div class="row">
-        <div class="col-lg-3 back-gray d-flex justify-content-center align-items-center">
-            <!-- Logo -->
-            <img src="{{ asset('images/logo.png') }}" alt="" width="170" class="">
+    <div class="row mt-4 px-3 pt-4">
+        <div class="col-lg-3 col-md-6 col-12 mx-auto mb-4 mb-lg-0 footer-content">
+            <!-- Shortcuts -->
+            <h6 class="fw-bold text-white mb-3 footer-list-title">Shortcuts</h6>
+            <ul class="footer-list">
+                <li class="text-gray px-2 mb-2">
+                    <a href="" class="text-reset footer-list-item">Top</a>
+                </li>
+                <li class="text-gray px-2 mb-2">
+                    <a href="" class="text-reset footer-list-item">About</a>
+                </li>
+                <li class="text-gray px-2 mb-2">
+                    <a href="" class="text-reset footer-list-item">Terms & Conditions</a>
+                </li>
+                <li class="text-gray px-2 mb-2">
+                    <a href="" class="text-reset footer-list-item">Subscribe</a>
+                </li>
+            </ul>
         </div>
-        <div class="col-lg-9 px-5 pt-4 pb-3">
-            <div class="row border border-gray border-3 rounded-pill ps-5">
-                <!-- Subscribe Now! -->
-                <div class="col-7 py-2">
-                    <p class="text-gray subscribe m-0">Your E-mail</p>
-                </div>
-                <div class="col-5 back-gray rounded-pill py-2">
-                    <p class="subscribe m-0 text-center">
-                        <a href="" class="text-blue">Subscribe Now!</a>
-                    </p>
-                </div>
-            </div>
-            <div class="row mt-4 px-3">
-                <div class="col-lg-4 footer-content">
-                    <!-- Shortcuts -->
-                    <h6 class="fw-bold text-white">Shortcuts</h6>
-                    <p class="text-gray px-2 footer-text">
-                        <a href="" class="text-reset">Top</a>
-                    </p>
-                    <p class="text-gray px-2 footer-text">
-                        <a href="" class="text-reset">About</a>
-                    </p>
-                    <p class="text-gray px-2 footer-text">
-                        <a href="" class="text-reset">Contact</a>
-                    </p>
-                    <p class="text-gray px-2 footer-text">
-                        <a href="" class="text-reset">Terms & Conditions</a>
-                    </p>
-                </div>
-                <div class="col-lg-4 footer-content">
-                    <!-- About -->
-                    <h6 class="fw-bold text-white">About</h6>
-                    <p class="text-gray px-2 footer-text">
-                        <a href="" class="text-reset">Our Company</a>
-                    </p>
-                    <p class="text-gray px-2 footer-text">
-                        <a href="" class="text-reset">Careers</a>
-                    </p>
-                </div>
-                <div class="col-lg-4 footer-content">
-                    <!-- Recent Post -->
-                    <h6 class="fw-bold text-white">Recent Post</h6>
-                    <p class="text-gray px-2 footer-text">
-                        <a href="" class="text-reset">News Title 1</a>
-                    </p>
-                    <p class="text-gray px-2 footer-text">
-                        <a href="" class="text-reset">News Title 1</a>
-                    </p>
-                    <p class="text-gray px-2 footer-text">
-                        <a href="" class="text-reset">News Title 1</a>
-                    </p>
-                    <p class="text-gray px-2 footer-text">
-                        <a href="" class="text-reset">News Title 1</a>
-                    </p>
-                </div>
-            </div>
-            <div class="row">
-                <!-- Copy right & Other apps -->
-                <div class="d-sm-flex justify-content-between border-top align-items-center mt-3 pt-3 px-3">
-                    <!-- Left -->
-                    <div>
-                        <span class="text-gray">All rights reserved 2022</span>
-                    </div>
-
-                    <!-- Right -->
-                    <div>
-                        <a href="" class="text-white sns-logo">
-                            <i class="fa-brands fa-twitter"></i>
-                        </a>
-                        <a href="" class="text-white ms-4 sns-logo">
-                            <i class="fa-brands fa-youtube"></i>
-                        </a>
-                        <a href="" class="text-white ms-4 sns-logo">
-                            <i class="fa-brands fa-linkedin"></i>
-                        </a>
-                        <a href="" class="text-white ms-4 sns-logo">
-                            <i class="fa-brands fa-facebook-f"></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
+        <div class="col-lg-3 col-md-6 col-12 mx-auto mb-4 mb-lg-0 footer-content">
+            <!-- About -->
+            <h6 class="fw-bold text-white mb-3 footer-list-title">About</h6>
+            <ul class="footer-list">
+                <li class="text-gray px-2 mb-2">
+                    <a href="" class="text-reset footer-list-item">Our Company</a>
+                </li>
+                <li class="text-gray px-2 mb-2">
+                    <a href="" class="text-reset footer-list-item">Careers</a>
+                </li>
+            </ul>
+        </div>
+        <div class="col-lg-3 col-md-6 col-12 mx-auto mb-4 mb-lg-0 footer-content">
+            <!-- Recent Post -->
+            <h6 class="fw-bold text-white mb-3 footer-list-title">Recent Post</h6>
+            <ul class="footer-list">
+                <li class="text-gray px-2 mb-2">
+                    <a href="" class="text-reset footer-list-item">News Title 1</a>
+                </li>
+                <li class="text-gray px-2 mb-2">
+                    <a href="" class="text-reset footer-list-item">News Title 1</a>
+                </li>
+                <li class="text-gray px-2 mb-2">
+                    <a href="" class="text-reset footer-list-item">News Title 1</a>
+                </li>
+                <li class="text-gray px-2 mb-2">
+                    <a href="" class="text-reset footer-list-item">News Title 1</a>
+                </li>
+            </ul>
+        </div>
+        <div class="col-lg-3 col-md-6 col-12 mx-auto mb-4 mb-lg-0 footer-content">
+            <!-- Recent Post -->
+            <h6 class="fw-bold text-white mb-3 footer-list-title">Contact</h6>
+            <ul class="footer-list">
+                <li class="text-gray px-2 mb-2">
+                    <a href="" class="text-reset footer-list-item"><i class="fa-solid fa-house me-2"></i>New York, NY 10012, US</a>
+                </li>
+                <li class="text-gray px-2 mb-2">
+                    <a href="" class="text-reset footer-list-item"><i class="fa-solid fa-envelope me-2"></i>info@example.com</a>
+                </li>
+                <li class="text-gray px-2 mb-2">
+                    <a href="" class="text-reset footer-list-item"><i class="fa-solid fa-phone me-2"></i>+ 01 234 567 88</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!-- Copy right & Other apps -->
+    <div class="d-sm-flex justify-content-between border-top align-items-center mt-3 pt-1 px-3">
+        <!-- Left -->
+        <div>
+            <a href=""><img src="{{ asset('images/logo3.png') }}" alt="" width="90"></a>
+            <span class="text-gray">All rights reserved 2022</span>
+        </div>
+        <!-- Right -->
+        <div class="text-center">
+            <a href="" class="text-white sns-logo">
+                <i class="fa-brands fa-twitter"></i>
+            </a>
+            <a href="" class="text-white ms-3 sns-logo">
+                <i class="fa-brands fa-youtube"></i>
+            </a>
+            <a href="" class="text-white ms-3 sns-logo">
+                <i class="fa-brands fa-linkedin"></i>
+            </a>
+            <a href="" class="text-white ms-3 sns-logo">
+                <i class="fa-brands fa-facebook-f"></i>
+            </a>
         </div>
     </div>
 </div>
