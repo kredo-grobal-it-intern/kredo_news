c91812b1816d14bcec11e6ab01e1a36f7795aa22
harufukuda <harufukuda14@gmail.com>
2022-09-21 21:20:12 +0900

    make top page responsive

--- resources/views/user/news/layouts/top_news.blade.php
+++ resources/views/user/news/layouts/top_news.blade.php
@@ -1,15 +1,15 @@
+<div class="col-md-7 order-md-2 col-12">
+    <!-- news image -->
+    <a href="{{ route('news.show', $country_news['latest']->id) }}"><img src="{{ asset('images/news/' . $country_news['latest']->image) }}" alt="{{ $country_news['latest']->image }}" class="w-100 top-image"></a>
+</div>
 <!-- top news -->
-<div class="col-5">
+<div class="col-md-5 order-md-1 col-12">
     <!-- heading & description & reaction -->
-    <h3 class="fw-bold top-heading"><a href="{{ route('news.show', $country_news['latest']->id) }}" class="text-danger text-decoration-none">{{ $country_news['latest']->title }}</a></h3>
+    <h3 class="fw-bold mt-3 mt-md-0 top-heading"><a href="{{ route('news.show', $country_news['latest']->id) }}" class="text-danger text-decoration-none">{{ $country_news['latest']->title }}</a></h3>
     <p class="mt-3 top-description">{{ $country_news['latest']->description }}</p>
-    <div class="d-flex mt-3 status">
+    <div class="d-flex mt-1 mt-md-3 status">
         @include('user.news.feature.reaction', ['news' => $country_news['latest']])
         @include('user.news.feature.comment', ['news' => $country_news['latest']])
         @include('user.news.feature.bookmark', ['news' => $country_news['latest']])
     </div>
-</div>
-<div class="col-7">
-    <!-- news image -->
-    <a href="{{ route('news.show', $country_news['latest']->id) }}"><img src="{{ asset('images/news/' . $country_news['latest']->image) }}" alt="{{ $country_news['latest']->image }}" class="w-100 top-image"></a>
 </div>
