b6631f40376eb50db96cd9d6b877ea1cc4e3e81d
harufukuda <harufukuda14@gmail.com>
2022-09-23 21:31:18 +0900

    move script tag

--- resources/views/user/profile/show/likes.blade.php
+++ resources/views/user/profile/show/likes.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
 @section('title','Like News')
+@section('style')
 <link href="{{ mix('css/news.css') }}" rel="stylesheet">
+@endsection
+@section('script_footer')
+<script src="{{ mix('js/_ajaxfollow.js') }}" defer></script>
+@endsection
 @section('content')
 <div class="container">
     @include('user.profile.components.user')
