02bec647e3833690852497757bb1d72c1e4247bd
Seiichiro <skyblue1011@icloud.com>
2022-09-24 15:48:38 +0900

    minor fix

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -48,7 +48,7 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      *
-     */
+     **/
     protected function validator(array $data)
     {
         return Validator::make($data, [
