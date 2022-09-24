06851957773df6b3da6a73eae8e1aabe5e515373
harufukuda <harufukuda14@gmail.com>
2022-09-24 16:03:00 +0900

    fix a bit

--- app/Http/Controllers/User/BookmarkController.php
+++ app/Http/Controllers/User/BookmarkController.php
@@ -13,15 +13,15 @@ class BookmarkController extends Controller
         $user = Auth::user();
         $user_id = $user->id;
         $news_id = $request->news_id;
-        $bookmark = $user->bookmarks->filter(function ($bookmark) use ($user_id, $news_id) {
+        $bookmarks = $user->bookmarks;
+        $bookmark = $bookmarks->filter(function ($bookmark) use ($user_id, $news_id) {
             return $bookmark->pivot->news_id == $news_id && $bookmark->pivot->user_id == $user_id;
         })->first();
-        $bookmarks = $user->bookmarks();
 
         if (!$bookmark) {
-            $bookmarks->attach($news_id);
+            $user->bookmarks()->attach($news_id);
         } else {
-            $bookmarks->detach($news_id);
+            $user->bookmarks()->detach($news_id);
         }
         return response()->json();
     }
