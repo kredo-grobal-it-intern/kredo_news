41593dee0bf44aecbdf86c68e430959d02279788
harufukuda <harufukuda14@gmail.com>
2022-09-24 15:18:38 +0900

    modify BookmarkController

--- app/Http/Controllers/User/BookmarkController.php
+++ app/Http/Controllers/User/BookmarkController.php
@@ -12,12 +12,17 @@ class BookmarkController extends Controller
     public function bookmark(Request $request)
     {
         $user = Auth::user();
+        $user_id = $user->id;
         $news_id = $request->news_id;
-        $bookmark = DB::table('bookmarks')->where('user_id', $user->id)->where('news_id', $news_id)->first();
+        $bookmark = $user->bookmarks->filter(function($bookmark) use($user_id, $news_id) {
+            return $bookmark->pivot->news_id == $news_id && $bookmark->pivot->user_id == $user_id;
+        })->first();
+        $bookmarks = $user->bookmarks();
+
         if (!$bookmark) {
-            $user->bookmarks()->attach($news_id);
+            $bookmarks->attach($news_id);
         } else {
-            $user->bookmarks()->detach($news_id);
+            $bookmarks->detach($news_id);
         }
         return response()->json();
     }
