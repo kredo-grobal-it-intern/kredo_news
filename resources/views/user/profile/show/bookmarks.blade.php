06f0bbc6cc359a1ef51d38af1123dfd35eafb8f5
harufukuda <harufukuda14@gmail.com>
2022-09-27 14:32:39 +0900

    apply css to bookmark page

--- resources/views/user/profile/show/bookmarks.blade.php
+++ resources/views/user/profile/show/bookmarks.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 @section('title','Bookmark News')
+@section('style')
 <link href="{{ mix('css/news.css') }}" rel="stylesheet">
+<link href="{{ mix('css/profile.css') }}" rel="stylesheet">
+@endsection
 @section('content')
 <div class="container">
     @include('user.profile.components.user')
