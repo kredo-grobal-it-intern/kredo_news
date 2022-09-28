6c162fe113ba7f77983a8990af2f82b02be717fb
harufukuda <harufukuda14@gmail.com>
2022-09-28 21:39:03 +0900

    phpcbf

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -10,7 +10,6 @@ use App\Models\User;
 use App\Models\Source;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
