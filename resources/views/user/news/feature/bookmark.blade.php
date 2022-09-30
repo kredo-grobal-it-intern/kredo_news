c259c77670034f5e34c24cb549287b9cedfe1a7c
harufukuda <harufukuda14@gmail.com>
2022-09-30 23:01:41 +0900

    edit class for styling

--- resources/views/user/news/feature/bookmark.blade.php
+++ resources/views/user/news/feature/bookmark.blade.php
@@ -1,9 +1,9 @@
 @if (Auth::check() && $news->isBookmarked())
-    <p>
-        <i class="bookmark fa-solid fa-bookmark bookmark-toggle text-success" data-newsid="{{ $news->id }}"></i>
+    <p class="bookmark">
+        <i class="fa-solid fa-bookmark bookmark-toggle text-success" data-newsid="{{ $news->id }}"></i>
     </p>
 @else
-    <p>
-        <i class="bookmark fa-regular fa-bookmark bookmark-toggle" @guest data-bs-toggle="modal" data-bs-target="#feature" @endguest data-newsid="{{ $news->id }}"></i>
+    <p class="bookmark">
+        <i class="fa-regular fa-bookmark bookmark-toggle" @guest data-bs-toggle="modal" data-bs-target="#feature" @endguest data-newsid="{{ $news->id }}"></i>
     </p>
 @endif
--- resources/views/user/news/feature/reaction.blade.php
+++ resources/views/user/news/feature/reaction.blade.php
@@ -1,25 +1,25 @@
 @if (Auth::check() && $news->isLiked())
-    <p class="me-2">
+    <p class="reaction">
         <span class="upCount">{{ $news->getLike()->count() }}</span>
-        <i class="reaction fa-regular fa-thumbs-up up-toggle text-primary" data-newsid="{{ $news->id }}"></i>
+        <i class="fa-regular fa-thumbs-up up-toggle text-primary" data-newsid="{{ $news->id }}"></i>
     </p>
 @else
-    <p class="me-2">
+    <p class="reaction">
         <span class="upCount">{{ $news->getLike()->count() }}</span>
-        <i class="reaction fa-regular fa-thumbs-up up-toggle"
+        <i class="fa-regular fa-thumbs-up up-toggle"
             @guest data-bs-toggle="modal" data-bs-target="#feature" @endguest
             data-newsid="{{ $news->id }}"></i>
     </p>
 @endif
 @if (Auth::check() && $news->isDisliked())
-    <p class="me-2">
+    <p class="reaction">
         <span class="downCount">{{ $news->getDislike()->count() }}</span>
-        <i class="reaction fa-regular fa-thumbs-down down-toggle text-primary" data-newsid="{{ $news->id }}"></i>
+        <i class="fa-regular fa-thumbs-down down-toggle text-primary" data-newsid="{{ $news->id }}"></i>
     </p>
 @else
-    <p class="me-2">
+    <p class="reaction">
         <span class="downCount">{{ $news->getDislike()->count() }}</span>
-        <i class="reaction fa-regular fa-thumbs-down down-toggle"
+        <i class="fa-regular fa-thumbs-down down-toggle"
             @guest  data-bs-toggle="modal" data-bs-target="#feature" @endguest
             data-newsid="{{ $news->id }}"></i>
     </p>
