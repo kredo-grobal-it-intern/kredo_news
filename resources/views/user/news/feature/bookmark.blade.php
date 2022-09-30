3274dc2222a04fbb49d9f3776f2891de39e41239
Seiichiro <skyblue1011@icloud.com>
2022-09-30 19:26:26 +0900

    Add verify modal

--- resources/views/user/news/feature/bookmark.blade.php
+++ resources/views/user/news/feature/bookmark.blade.php
@@ -2,6 +2,10 @@
     <p>
         <i class="bookmark fa-solid fa-bookmark bookmark-toggle text-success" data-newsid="{{ $news->id }}"></i>
     </p>
+@elseif (Auth::check() && !Auth::user()->email_verified_at)
+    <p>
+        <i class="bookmark fa-regular fa-bookmark bookmark-toggle" data-bs-toggle="modal" data-bs-target="#verify"  data-newsid="{{ $news->id }}"></i>
+    </p>
 @else
     <p>
         <i class="bookmark fa-regular fa-bookmark bookmark-toggle" @guest data-bs-toggle="modal" data-bs-target="#feature" @endguest data-newsid="{{ $news->id }}"></i>
