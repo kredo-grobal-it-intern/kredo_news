98dd9c909b4ea0a952a5b64453056e2611dbcc03
harufukuda <harufukuda14@gmail.com>
2022-09-30 23:02:01 +0900

    delete not needed file

--- webpack.mix.js
+++ webpack.mix.js
@@ -21,7 +21,6 @@ const mix = require('laravel-mix');
  .js('resources/js/_comment_like.js', 'public/js')
  .sass('resources/sass/app.scss', 'public/css')
  .sass('resources/sass/admin.scss', 'public/css')
- .sass('resources/sass/news.scss', 'public/css')
  .sass('resources/sass/profile.scss', 'public/css')
  .sass('resources/sass/profile_edit.scss', 'public/css')
  .sass('resources/sass/login.scss', 'public/css')
