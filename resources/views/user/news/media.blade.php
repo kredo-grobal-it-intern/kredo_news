9435b60e2f2d3525c51df0f67276fe67de5c44ea
harufukuda <harufukuda14@gmail.com>
2022-10-03 23:22:44 +0900

    integrate css file and edit header font

--- resources/views/user/news/media.blade.php
+++ resources/views/user/news/media.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.app')
 @section('style')
-<link href="{{ mix('css/country.css') }}" rel="stylesheet">
+<link href="{{ mix('css/filtered_page.css') }}" rel="stylesheet">
 <link href="{{ mix('css/news_list.css') }}" rel="stylesheet">
 @endsection
 @section('title',"Media" . $media->country->name)
 @section('content')
 <div class="container">
-    <div class="row justify-content-center back">
+    <div class="header-section">
         @if ($media->country->image)
-        <img src="{{asset('images/countries/' . $media->country->image)}}" alt="{{$media->country->name}}">
+            <img src="{{asset('images/countries/' . $media->country->image)}}" alt="{{$media->country->name}}" class="header-section-image">
         @else
-        <img src="{{asset('images/country.jpg')}}" alt="{{$media->country->name}}">
+            <img src="{{asset('images/country.jpg')}}" alt="{{$media->country->name}}" class="header-section-image">
         @endif
-        <h1 class="text-center fw-bold country_title">{{$media->country->name}}</h1>
+        <h1 class="text-center fw-bold header-section-title">{{$media->country->name}}</h1>
     </div>
 </div>
 <div class="container">
