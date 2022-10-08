6e55707e5b456768def04f5cfe2eb1538380b457
harufukuda <harufukuda14@gmail.com>
2022-10-08 16:45:03 +0900

    apply DataTables to admin's news table

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -19,10 +19,11 @@ class NewsController extends Controller
     const DRAFT = 2;
 
     const LOCAL_STORAGE_FOLDER = 'public/images/news';
-    
+
     public function index()
     {
-        $all_news = News::orderBy('published_at', 'desc')->withTrashed()->paginate(10);
+        // $all_news = News::orderBy('published_at', 'desc')->withTrashed()->paginate(10);
+        $all_news = News::withTrashed()->get();
         return view('admin.news.list')
                     ->with('all_news', $all_news);
     }
@@ -37,7 +38,7 @@ class NewsController extends Controller
         $good_news     = News::getTopGoodNewsList();
         $bad_news      = News::getWorstBadNewsList();
         $bookmark_news = News::getTopBookmarkNewsList();
-        
+
         return view('admin.dashboard')
                 ->with('news', $news)
                 ->with('users', $users)
