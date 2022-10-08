56a0d99042a2142bdc065b5ae75a5acdbaa340f6
harufukuda <harufukuda14@gmail.com>
2022-10-08 10:51:22 +0900

    apply jquery to edit news page

---
+++ resources/js/_news_edit.js
@@ -0,0 +1,9 @@
+$(function () {
+    $('#image').on('change', function(e) {
+        let reader = new FileReader();
+        reader.onload = function(e) {
+            $('#showImage').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(e.target.files['0']);
+    });
+});
--- resources/views/admin/news/edit.blade.php
+++ resources/views/admin/news/edit.blade.php
@@ -6,6 +6,10 @@
 <link href="{{ mix('css/admin/create.css') }}" rel="stylesheet">
 @endsection
 
+@section('script')
+<script src="{{ mix('js/_news_edit.js') }}" defer></script>
+@endsection
+
 @section('content')
     @include('admin.news.layouts.create')
 @endsection
