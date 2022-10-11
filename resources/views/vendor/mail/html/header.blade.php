54dbee347cd4a6e8bb6b05cd49a141c3fc6d69e7
Seiichiro <skyblue1011@icloud.com>
2022-10-11 21:14:04 +0900

    Logo changed

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            <img src="{{ asset('images/logo.webp') }}" class="logo" alt="CCC Logo">
+            <img src="{{ asset('images/logo3.PNG') }}" class="logo" alt="CCC Logo">
         </a>
     </td>
 </tr>
