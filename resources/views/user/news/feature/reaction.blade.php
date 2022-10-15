b4257ae13f326380eb07b38d6868d790f10dfede
harufukuda <harufukuda14@gmail.com>
2022-10-15 16:59:12 +0900

    modifid bookmark component

--- resources/views/user/news/feature/reaction.blade.php
+++ resources/views/user/news/feature/reaction.blade.php
@@ -4,21 +4,24 @@
         <span class="upCount">{{ number_format($news->getLike()->count()) }}</span>
         <i class="fa-regular fa-thumbs-up"
             data-bs-toggle="modal" data-bs-target="#feature"
-            data-newsid="{{ $news->id }}"></i>
+            data-newsid="{{ $news->id }}">
+        </i>
     </p>
 @else
     @if (Auth::user()->hasVerifiedEmail())
         <p class="reaction">
             <span class="upCount">{{ number_format($news->getLike()->count()) }}</span>
             <i class="fa-regular fa-thumbs-up up-toggle @if($news->isLiked()) text-primary @endif"
-                data-newsid="{{ $news->id }}"></i>
+                data-newsid="{{ $news->id }}">
+            </i>
         </p>
     @else
         <p class="reaction">
             <span class="upCount">{{ number_format($news->getLike()->count()) }}</span>
             <i class="fa-regular fa-thumbs-up"
                 data-bs-toggle="modal" data-bs-target="#verify"
-                data-newsid="{{ $news->id }}"></i>
+                data-newsid="{{ $news->id }}">
+            </i>
         </p>
     @endif
 @endguest
@@ -29,21 +32,24 @@
         <span class="downCount">{{ number_format($news->getDislike()->count()) }}</span>
         <i class="fa-regular fa-thumbs-down"
             data-bs-toggle="modal" data-bs-target="#feature"
-            data-newsid="{{ $news->id }}"></i>
+            data-newsid="{{ $news->id }}">
+        </i>
     </p>
 @else
     @if (Auth::user()->hasVerifiedEmail())
         <p class="reaction">
             <span class="downCount">{{ number_format($news->getDislike()->count()) }}</span>
             <i class="fa-regular fa-thumbs-down down-toggle @if($news->isDisliked()) text-danger @endif"
-                data-newsid="{{ $news->id }}"></i>
+                data-newsid="{{ $news->id }}">
+            </i>
         </p>
     @else
         <p class="reaction">
             <span class="downCount">{{ number_format($news->getDislike()->count()) }}</span>
             <i class="fa-regular fa-thumbs-down"
                 data-bs-toggle="modal" data-bs-target="#verify"
-                data-newsid="{{ $news->id }}"></i>
+                data-newsid="{{ $news->id }}">
+            </i>
         </p>
     @endif
 @endguest
