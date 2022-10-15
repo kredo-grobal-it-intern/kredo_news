e8ad4a37527f2e49566359f64f3136a2d23fc4ef
harufukuda <harufukuda14@gmail.com>
2022-10-15 16:48:04 +0900

    modify reaction component

--- resources/views/user/news/feature/reaction.blade.php
+++ resources/views/user/news/feature/reaction.blade.php
@@ -1,42 +1,51 @@
-@if (Auth::check() && $news->isLiked())
+<!-- Like button -->
+@guest
     <p class="reaction">
         <span class="upCount">{{ number_format($news->getLike()->count()) }}</span>
-        <i class="fa-regular fa-thumbs-up up-toggle text-primary" data-newsid="{{ $news->id }}"></i>
-    </p>
-@elseif (Auth::check() && !Auth::user()->hasVerifiedEmail())
-    <p class="me-2">
-        <span class="upCount">{{ number_format($news->getLike()->count()) }}</span>
-        <i class="reaction fa-regular fa-thumbs-up up-toggle"
-             data-bs-toggle="modal" data-bs-target="#verify"
+        <i class="fa-regular fa-thumbs-up"
+            data-bs-toggle="modal" data-bs-target="#feature"
             data-newsid="{{ $news->id }}"></i>
     </p>
 @else
-    <p class="reaction">
-        <span class="upCount">{{ number_format($news->getLike()->count()) }}</span>
-        <i class="fa-regular fa-thumbs-up up-toggle"
-            @guest data-bs-toggle="modal" data-bs-target="#feature" @endguest
-            data-newsid="{{ $news->id }}"></i>
-    </p>
-@endif
-@if (Auth::check() && $news->isDisliked())
+    @if (Auth::user()->hasVerifiedEmail())
+        <p class="reaction">
+            <span class="upCount">{{ number_format($news->getLike()->count()) }}</span>
+            <i class="fa-regular fa-thumbs-up up-toggle @if($news->isLiked()) text-primary @endif"
+                data-newsid="{{ $news->id }}"></i>
+        </p>
+    @else
+        <p class="reaction">
+            <span class="upCount">{{ number_format($news->getLike()->count()) }}</span>
+            <i class="fa-regular fa-thumbs-up"
+                data-bs-toggle="modal" data-bs-target="#verify"
+                data-newsid="{{ $news->id }}"></i>
+        </p>
+    @endif
+@endguest
+
+<!-- Dislike button -->
+@guest
     <p class="reaction">
         <span class="downCount">{{ number_format($news->getDislike()->count()) }}</span>
-        <i class="fa-regular fa-thumbs-down down-toggle text-primary" data-newsid="{{ $news->id }}"></i>
-    </p>
-@elseif (Auth::check() && !Auth::user()->email_verified_at)
-    <p class="me-2">
-        <span class="downCount">{{ number_format($news->getDislike()->count()) }}</span>
-        <i class="reaction fa-regular fa-thumbs-down down-toggle"
-            data-bs-toggle="modal" data-bs-target="#verify"
+        <i class="fa-regular fa-thumbs-down"
+            data-bs-toggle="modal" data-bs-target="#feature"
             data-newsid="{{ $news->id }}"></i>
     </p>
 @else
-    <p class="reaction">
-        <span class="downCount">{{ number_format($news->getDislike()->count()) }}</span>
-        <i class="fa-regular fa-thumbs-down down-toggle"
-            @guest  data-bs-toggle="modal" data-bs-target="#feature" @endguest
-            data-newsid="{{ $news->id }}"></i>
-    </p>
-@endif
-@include('user/news/modal/reaction')
-@include('user/news/modal/verify')
+    @if (Auth::user()->hasVerifiedEmail())
+        <p class="reaction">
+            <span class="downCount">{{ number_format($news->getDislike()->count()) }}</span>
+            <i class="fa-regular fa-thumbs-down down-toggle @if($news->isDisliked()) text-danger @endif"
+                data-newsid="{{ $news->id }}"></i>
+        </p>
+    @else
+        <p class="reaction">
+            <span class="downCount">{{ number_format($news->getDislike()->count()) }}</span>
+            <i class="fa-regular fa-thumbs-down"
+                data-bs-toggle="modal" data-bs-target="#verify"
+                data-newsid="{{ $news->id }}"></i>
+        </p>
+    @endif
+@endguest
+@include('user.news.modal.reaction')
+@include('user.news.modal.verify')
