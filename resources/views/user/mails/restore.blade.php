62e635dcf36f9ca4d84eb8121c9447f39799a9d9
Seiichiro <skyblue1011@icloud.com>
2022-10-24 21:53:03 +0900

    Fix a typo and reactivate function error

--- resources/views/user/mails/restore.blade.php
+++ resources/views/user/mails/restore.blade.php
@@ -1,16 +1,18 @@
 @component('mail::message')
 <h1>Dear {{$user->username}}</h1>
-<h2>If you want to reactivate your account, You will have a month to restore</h2>
+<p>If you want to reactivate your account, You will have 30 days to restore</p>
 @component('mail::button', ['url' => $url])
     Reactivate Account
 @endcomponent
+<p>This reactivation will expire in 30 days.</p>
 <p>
-    Thanks for useing our app.
+    Thanks for using our app.
 </p>
 <p>
     Regards,<br>
     CCC news
 </p>
+<hr>
 @component('mail::footer',['url => $url'])
 <p>
     If you're having trouble clicking the "Reactivate Account" button, copy and paste the URL below into your web browser:{!! $url !!}
