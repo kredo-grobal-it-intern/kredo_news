598f29014bfbd66a2fa0f447866cc78c78b60500
harufukuda <harufukuda14@gmail.com>
2022-10-26 19:56:18 +0900

    add RefreshDatabase and setUp to RestoreMailTest

--- tests/Feature/RestoreMailTest.php
+++ tests/Feature/RestoreMailTest.php
@@ -8,19 +8,25 @@ use Tests\TestCase;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\RestoreMail;
 use App\Models\User;
+use Database\Seeders\CountrySeeder;
 
 class RestoreMailTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_restoreMail()
+    use RefreshDatabase;
+
+    private $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->seed(CountrySeeder::class);
+        $this->user = User::factory()->create();
+    }
+
+    public function test_user_can_receive_email_for_restoration()
     {
         Mail::fake();
-        $user = User::factory()->create();
-        Mail::to($user->email)->send(new RestoreMail($user));
+        Mail::to($this->user->email)->send(new RestoreMail($this->user));
         Mail::assertSent(RestoreMail::class);
     }
 }
