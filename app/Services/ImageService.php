fe57f77a5c60b4b509e1ec624557f3e0453312fe
Seiichiro <skyblue1011@icloud.com>
2022-10-29 13:52:49 +0900

    path fix

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -17,9 +17,9 @@ class ImageService
 
         $file_name = time() . '.' . 'webp';
 
-        $path = storage_path('app/'. $storage);
+        $path = storage_path($storage);
         // dd($path);
-        $resize_image->save($path . $file_name);
+        $resize_image->save($file_name);
 
         return $file_name;
     }
